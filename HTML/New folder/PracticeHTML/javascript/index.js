a80bffc0bc4c2b86f26ef932617ec1d5bf3a68a7
speddirajubcj <speddiraju.bcj@gmail.com>
2016-09-15 14:52:19 -0500

    initial html commit

---
+++ HTML/New folder/PracticeHTML/javascript/index.js
@@ -0,0 +1,19 @@
+function add(a, b) {
+    document.write(a,b);
+   // return a + b;
+}
+
+function sub(a, b) {
+    return a - b;
+}
+
+function multiply(a, b) {
+    return a * b;
+}
+
+function divide(a, b) {
+    return a / b;
+}
+function modulus(a, b) {
+    return a % b;
+}
